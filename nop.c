1cb6c28545e26f5d8f986c6d72927a5619312427
Yahya Ahmed <yahya.clash8810@gmail.com>
2024-09-28 11:27:00 -0700

    nop

---
+++ nop.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * nop - doesn’t do anything.
+ * @stack: pointer to a pointer to a stack. 
+ * @line_number: the line being parsed 
+ *
+ * Return: void 
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+(void)stack;
+(void)line_number;
+return;
+} 
